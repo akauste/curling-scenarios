cdb9d15635bdb380b31a0359897b7994dfcc1ab6
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-04-29 14:45:19 +0300

    Add testid's

--- src/components/sheet/DoublesMarkings.js
+++ src/components/sheet/DoublesMarkings.js
@@ -6,12 +6,12 @@ const DoublesMarkings = props => {
   const doublesMarking = [];
   for(let x=-1; x < 2; x++) {
     for(let y=-1; y < 2; y++) {
-      doublesMarking.push(<circle key={`dm${x}${y}`} cx={ doublesX+x*107-(x*(y)*2)} cy={direction*(doublesY+y*91.4)} r={1.6} stroke='#393' fill='silver' strokeWidth='1' />);
+      doublesMarking.push(<circle key={`dm${x}${y}`} cx={ doublesX+x*107-(x*(y)*2)} cy={direction*(doublesY+y*91.4)} r={1.6} stroke='#393' fill='silver' strokeWidth='1' data-testid='dot' />);
       //[backgap+183*2+228.6-91.4,])
     }
   }
 
-  return (<g id="doubles-markings">
+  return (<g data-testid="doubles-markings">
     { doublesMarking.map(i => i) }
   </g>);
 }
