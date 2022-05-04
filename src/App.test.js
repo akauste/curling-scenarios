6a234aa181cf171a9e9f8d28c973f2d56b83e0e0
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-05-04 09:32:25 +0300

    Basic tests that App can render

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+//import { render, screen } from '@testing-library/react';
+import { render, screen } from './utils/test-utils';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { TouchBackend } from 'react-dnd-touch-backend'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  test('App is rendering', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const headerElement = screen.getByText(/Curling Scenarios/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  test('App is rendering board', () => {
+    render(<MemoryRouter initialEntries={["/board"]}><DndProvider backend={TouchBackend}><App /></DndProvider></MemoryRouter>);
+    const headerElement = screen.getByText(/Curling Scenarios/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
 });
+// test('not yet implemented', () => {});
