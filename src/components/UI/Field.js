586d7e8949becfbbeb650bee62eaedf6b777e7d5
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-05-19 12:10:07 +0300

    Add error handling to Field

--- src/components/UI/Field.js
+++ src/components/UI/Field.js
@@ -1,10 +1,13 @@
+import classes from './Field.module.css';
+
 const Field = props => {
-  const {label, id} = props;
-  return <div className="field">
+  const {label, id, error, valid, errorMessage} = props;
+  return <div className={`${classes.field} ${ error ? 'error' : valid ? 'valid' : '' }`}>
     <label htmlFor={id}>{label}</label>
-    <div className="controls">
-      {props.children}
+    <div className={`${classes.controls} ${ error ? classes.error : valid ? classes.valid : ''}`}>
+      {props.children} 
     </div>
+    { error && <p className={classes.errorText}>{ errorMessage }</p> }
   </div>;
 };
 export default Field;
