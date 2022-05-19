becc5e22b764c3bd0bc32c5002d0ce7943fc6e01
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-05-19 12:08:33 +0300

    Add api.ts & ts.config.json

---
+++ src/lib/api.ts
@@ -0,0 +1,48 @@
+type HttpMethod =
+  "GET" |
+  "POST" |
+  "PUT" |
+  "DELETE" |
+  "PATCH";
+
+export const apiCall = async (
+  path: string,
+  method: HttpMethod,
+  data: any,
+  token: string | null,
+) => {
+  const headers: any = {Accept: 'application/json'};
+  if (token) {
+    headers["Authorization"] = "Bearer " + token;
+  }
+  try {
+    let body;
+    if(data instanceof FormData) { 
+      const convertedFormEntries = Array.from(
+        data,
+        ([key, value]) => (
+          [key, typeof value === 'string' ? value : value.name]
+        ),
+      );
+      body = new URLSearchParams(convertedFormEntries);
+    }
+    else {
+      headers["Content-Type"] = "application/json";
+      body = JSON.stringify(data);
+    }
+    const response = await fetch("http://localhost:3001" + path, {
+      method: method,
+      headers: headers,
+      body: body,
+    });
+    const json = await response.json();
+    console.log("API RETURNS:", {
+      res: response,
+      status: response.status,
+      data: json,
+    });
+    return { res: response, status: response.status, data: json };
+  } catch (err) {
+    console.log("Catching an error!", err);
+  }
+};
