17753a222a0a55b9d33097d35212fc9aff4b347d
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-06-09 11:28:49 +0300

    Use the toaster component

--- src/components/scenario/Situation.js
+++ src/components/scenario/Situation.js
@@ -1,11 +1,13 @@
-import { useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { apiCall } from "../../lib/api";
 import Field from "../UI/Field";
 
+import { toasterActions } from "../../store/toaster-slice";
+
 const Situation = props => {
   const {team1, team2} = props;
-  const [id, setId] = useState(props.init?.id || '');
+  const [id, setId] = useState(props.id || '');
   const [title, setTitle] = useState(props.init?.title || '');
   const [weAre, setWeAre] = useState(props.init?.weAre || team1);
   const [hammer, setHammer] = useState(props.init?.hammer || team1);
@@ -14,9 +16,18 @@ const Situation = props => {
   const [score, setScore] = useState(props.init?.score || '');
   const commentRef = useRef();
   const [status, setStatus] = useState(props.init?.id || 'new');
+  const dispatch = useDispatch();
 
   const sheet = useSelector(state => state.sheet);
   const stones = useSelector(state => state.stones);
+
+  useEffect(() => {
+    setId(props.id);
+    if(props.init) {
+      setHammer(props.init.hammer);
+      setWeAre(props.init.weAre);
+    }
+  }, [props.id, props.init]);
 
   const endsRemaining = () => {
     return gameLengthRef.current.value - end +1;
@@ -70,6 +81,50 @@ const Situation = props => {
   };
 
   const makeScenario = () => {
+    // Validate data:
+    const [we, they] = weAre === 'red' ? [1, 2] : [2, 1];
+    const ourStonesLeft = stones.stones.filter(s => !s.played && s.team === we).length;
+    const theirStonesLeft = stones.stones.filter(s => !s.played && s.team === they).length;
+    
+    console.log('Stones left:', ourStonesLeft, theirStonesLeft);
+    const errors = [];
+
+    dispatch(toasterActions.add({title: 'Test message', message: 'This is what a real success looks like!', type: 'success', autoRemove: false}));
+    dispatch(toasterActions.add({title: 'Test message', message: 'Warning, this is a WARNING!', type: 'warning'}));
+    dispatch(toasterActions.add({title: 'Test message', type: 'info', autoRemove: true, displayTime: 3000}));
+    dispatch(toasterActions.add({title: 'Test message', message: 'ERROR: Now it is broken!', type: 'error'}));
+
+    if(ourStonesLeft === 0) {
+      errors.push({
+        type: 'error',
+        title: 'Please check the stone positions',
+        message: 'Expected to have at least one stone to come for us, '
+        +' please check that you have some stones before hogline' });
+    }
+    else if(weAre === hammer && ourStonesLeft !== (theirStonesLeft + 1)) {
+      console.warn((weAre === hammer) + ' '+ ourStonesLeft +' !== '+ (theirStonesLeft + 1));
+      errors.push({
+        type: 'error',
+        title: 'Please check the stone positions',
+        message: 'Expected us to have 1 more stone left than opponent, '
+        +'please check that you have correct number of stones before the hogline. We are: '+ weAre });
+    }
+    else if(weAre !== hammer && ourStonesLeft !== theirStonesLeft) {
+      errors.push({
+        type: 'error',
+        title: 'Please check the stone positions',
+        message: 'Expected us to have the same number of stones left as the opponent, '
+        +'please check that you have correct number of stones before the hogline'});
+    }
+    if(end > 1 && typeof score_diff !== 'number') {
+      errors.push({
+        type: 'error',
+        title: 'Fix the score',
+        message: 'Set the score difference'});
+    }
+    console.error(errors);
+    errors.forEach(e => dispatch(toasterActions.add(e)) );
+    //dispatch();
     // Save as scenario ->
     // Turn edit mode to scenario =>
   };
